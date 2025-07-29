a3c0c0658078430e3294b2807a6e2408af5d7c53
Can Guvercin <mustafacanguvercin@gmail.com>
2025-07-30 00:09:02 +0300

    first commit

---
+++ src/pages/Products.js
@@ -0,0 +1,10 @@
+function ProductsPage() {
+  return (
+    <div>
+      <h1>Products Page</h1>
+      <p>Here you can find a list of our products.</p>
+    </div>
+  );
+}
+
+export default ProductsPage;
